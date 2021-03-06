4cca05c9b3ef06679210ebd8fd65b1a6e6f2b78f
Drake <j.drake.hoffman@gmail.com>
2021-03-06 10:48:07 -0500

    continue getting key

--- bw.sh
+++ bw.sh
@@ -5,6 +5,7 @@
 # - BW_CLIENTSECRET="alphabet_soup"
 # https://bitwarden.com/help/article/personal-api-key/
 
+bw logout
 # check BW_CLIENTID
 if [[ -z "${BW_CLIENTID}" ]]; then
     echo "BW_CLIENTID env var is not set. exiting..."
@@ -22,7 +23,11 @@ bw login --apikey
 
 echo "Unlock BitWarden Vault:"
 SESSION_TXT=$(bw unlock | tee)
-echo "Bitwarden session text:"
-echo "${SESSION_TEXT}"
+# echo "Bitwarden session text:"
+# echo "${SESSION_TXT}"
+
+SESSION_LINE=$(echo "${SESSION_TXT}" | grep "export BW_SESSION")
+echo "line containing the session key is:"
+echo "${SESSION_LINE}"
 
 bw logout
