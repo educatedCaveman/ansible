7bc93eec836cf6d272065ede1bb83d9b78e374be
Drake <j.drake.hoffman@gmail.com>
2021-07-10 09:52:49 -0400

    debugging

--- maintenance/PW_hostname.sh
+++ maintenance/PW_hostname.sh
@@ -7,34 +7,35 @@
 ANSIBLE_DIR='/home/drake/ansible/maintenance'
 
 # hosts for ansible
-HOSTS=( \
-    dev_api_LB \
-    dev_swarm_manager_01 \
-    dev_swarm_manager_02 \
-    dev_swarm_manager_03 \
-    dev_swarm_worker_01 \
-    dev_swarm_worker_02 \
-    dev_swarm_worker_03 \
-    dev_swarm_worker_04 \
-    prd_api_LB \
-    prd_swarm_manager_01 \
-    prd_swarm_manager_02 \
-    prd_swarm_manager_03 \
-    prd_swarm_worker_01 \
-    prd_swarm_worker_02 \
-    prd_swarm_worker_03 \
-    prd_swarm_worker_04 \
-    apt_cache \
-    apt_mirror \
-    nextcloud_vm \
-    moria \
-    origami \
-    influxDB_vm \
-    grafana_vm \
-    minio_vm \
-    graylog_vm \
-    syncthing_vm \
-)
+# HOSTS=( \
+#     dev_api_LB \
+#     dev_swarm_manager_01 \
+#     dev_swarm_manager_02 \
+#     dev_swarm_manager_03 \
+#     dev_swarm_worker_01 \
+#     dev_swarm_worker_02 \
+#     dev_swarm_worker_03 \
+#     dev_swarm_worker_04 \
+#     prd_api_LB \
+#     prd_swarm_manager_01 \
+#     prd_swarm_manager_02 \
+#     prd_swarm_manager_03 \
+#     prd_swarm_worker_01 \
+#     prd_swarm_worker_02 \
+#     prd_swarm_worker_03 \
+#     prd_swarm_worker_04 \
+#     apt_cache \
+#     apt_mirror \
+#     nextcloud_vm \
+#     moria \
+#     origami \
+#     influxDB_vm \
+#     grafana_vm \
+#     minio_vm \
+#     graylog_vm \
+#     syncthing_vm \
+# )
+HOSTS=(influxDB_vm grafana_vm minio_vm graylog_vm syncthing_vm)
 
 # check BW_CLIENTID
 if [[ -z "${BW_CLIENTID}" ]]; then
@@ -61,12 +62,15 @@ SESSION_KEY=${SESSION_LINE:20}
 export BW_SESSION=$SESSION_KEY
 
 #handle drake users
-for (( n=0; n<${#HOSTS[@]}; n++ ))
+for host in "${#HOSTS[@]}"
 do
-    echo "starting configuration of ${HOSTS[$n]}"
+    echo "starting configuration of $host"
 
     # get the full BitWarden item
-    BW_ITEM=$(bw get item "${HOSTS[$n]}")
+    BW_ITEM=$(bw get item "$host")
+    echo "BW_ITEM:"
+    echo "$BW_ITEM"
+    echo ""
 
     # filter out the password
     BW_PASS=$(echo "${BW_ITEM}" | jq '.login.password')
@@ -81,14 +85,14 @@ do
     LXC=${BW_LXC:1:-1}
 
     # run the playbook, passing in the secrets
-    ansible-playbook -l "${HOSTS[$n]}" "${ANSIBLE_DIR}/PW_hostname.yml" \
+    ansible-playbook -l "$host" "${ANSIBLE_DIR}/PW_hostname.yml" \
         --extra-vars mypass="${PASSWORD}" --extra-vars hn="${HOSTNAME}"
 
     # handle the root user for LXC containers
     if [ "${LXC}" == "true" ]
     then
         # echo "this is where we'd run the LXC playbook"
-        ansible-playbook -l "${HOSTS[$n]}" "${ANSIBLE_DIR}/LXC_root_setup.yml" \
+        ansible-playbook -l "$host" "${ANSIBLE_DIR}/LXC_root_setup.yml" \
             --extra-vars mypass="${PASSWORD}"
     fi
 
