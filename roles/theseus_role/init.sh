6487e5eab6dcd569d6771c181d8d81cc68052dc1
John Hoffman <j.drake.hoffman@gmail.com>
2024-06-12 14:01:54 -0400

    add fan control

--- roles/theseus_role/init.sh
+++ roles/theseus_role/init.sh
@@ -27,10 +27,13 @@ function yes_or_no {
         esac
     done
 }
-yes_or_no "have you enabled AUR and FlatPak?" && sudo pamac install konsave ansible google-chrome brother-hll2395dw direnv --no-confirm
+yes_or_no "have you enabled AUR and FlatPak?" && sudo pamac install konsave ansible google-chrome brother-hll2395dw direnv fw-fanctrl-ui-git --no-confirm
 # sudo pamac install konsave ansible google-chrome vscodium ultra-flat-icons-blue brother-hll2395dw --no-confirm
 flatpak install flathub tv.plex.PlexDesktop
 flatpak install flathub com.plexamp.Plexamp 
+
+#Fan control
+sudo systemctl enable --now fw-fanctrl.service
 
 # # SSH Key setup
 # # create ssh keys if they don't already exist
